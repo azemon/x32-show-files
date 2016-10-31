d69d842d204870c11225db02eb2038ea329dabba
Art Zemon <art@zemon.name>
2016-10-31 11:14:25 -0500

    Add support for cues, in addition to snippets. Also support 21 inputs, up from 16.

--- X32Snippets.py
+++ X32Snippets.py
@@ -3,19 +3,19 @@
 ################################################################################
 #
 # X32 Snippets
-# v1.0
 # Written by Simon Eves
+# Updated to support cues, in addition to snippets, by Art Zemon art@zemon.name https://CheerfulCurmudgeon.com/
 # Free for non-commercial use
 #
 ################################################################################
+
+VERSION = '1.1'
 
 ################################################################################
 # Imports
 ################################################################################
 
-import time
 import sys
-import string
 
 from pyexcel_ods import get_data
 
@@ -28,13 +28,15 @@ SHEET_NAME = "Sheet1"
 
 SKIP_ROWS = 3
 
-SCENE_COL = 0
-CUE_COL = 2
+SNIPPET_NUMBER_COL = 0
+SNIPPET_NAME_COL = 2
 
-FIRST_CHAN_COL = 4
+CUE_NUMBER_COL = 3
+
+FIRST_CHAN_COL = 5
 NUM_CHANS = 21
 
-FIRST_DCA_COL = 26
+FIRST_DCA_COL = 27
 NUM_DCAS = 8
 
 
@@ -51,131 +53,202 @@ NUM_DCAS = 8
 
 def ods_cell(d, r, c):
 
-	row = d[r]
-	try:
-		cell = row[c]
-	except IndexError:
-		cell = ''
-	#print "DEBUG: cell " + str(r) + "/" + str(c) + " is type " + str(type(cell))
-	if type(cell) == unicode:
-		cell = cell.encode('utf-8')
-	return cell
-	
+    row = d[r]
+    try:
+        cell = row[c]
+    except IndexError:
+        cell = ''
+    #print "DEBUG: cell " + str(r) + "/" + str(c) + " is type " + str(type(cell))
+    if type(cell) == unicode:
+        cell = cell.encode('utf-8')
+    return cell
+
+
+
+################################################################################
+# Cues
+################################################################################
+
+class Cue(object):
+    def __init__(self, cue_number, cue_name, snippet_number):
+        self.cue_number = cue_number
+        self.cue_name = cue_name
+        self.snippet_number = snippet_number
+        self.convert_cue_number()
+
+    def convert_cue_number(self):
+        """
+        convert a cue number as a string into an integer
+        valid inputs are of the form N or N.M or N.M.P where M and P can only be single digits
+        e.g., 1 or 1.2 or 1.2.3 but not 1.15
+        """
+        cue_integer = None
+        parts = self.cue_number.split('.')
+        for p in range(0, len(parts)):
+            parts[p] = int(parts[p])
+        if 1 == len(parts):
+            cue_integer = parts[0] * 100
+        elif 2 == len(parts):
+            if 10 <= parts[1]:
+                raise ValueError('Illegal cue number %s on snippet %s' % (self.cue_number, self.snippet_number))
+            cue_integer = parts[0] * 100 + parts[1] * 10
+        elif 3 == len(parts):
+            if 10 <= parts[1] or 10 <= parts[2]:
+                raise ValueError('Illegal cue number %s on snippet %s' % (self.cue_number, self.snippet_number))
+            cue_integer = parts[0] * 100 + parts[1] * 10 + parts[2]
+        else:
+            raise ValueError('Illegal cue number %s on snippet %s' % (self.cue_number, self.snippet_number))
+        self.cue_number = cue_integer
+
+    def shw_file_line(self, index):
+        cue_name = self.cue_name.replace('"', '')[:16]
+        return 'cue/%s %s "%s" 0 -1 %s 0 1 0 0\n' % \
+              (str(index).zfill(3), self.cue_number, cue_name, self.snippet_number)
+
 
 ################################################################################
 # Main
-################################################################################	
-		
+################################################################################
+
 if __name__ == "__main__":
 
-	print "#####################"
-	print "# X32 Snippets v1.0 #"
-	print "#####################"
-	
-	#
-	# validate command-line parameters
-	#
-	
-	if len(sys.argv) != 3:
-		print "";
-		print "Usage: X32Snippets.py <ods_file_name> <show_name>"
-		sys.exit(0)
-		
-	#
-	# get command line parameters
-	#
+    print "#####################"
+    print "# X32 Snippets v%s #" % (VERSION,)
+    print "#####################"
 
-	ods_file_name = sys.argv[1]
-	show_name = sys.argv[2]
+    #
+    # validate command-line parameters
+    #
 
-	#
-	# process file
-	#
-	
-	# read the file
-	ods = get_data(ods_file_name)[SHEET_NAME]
-	
-	# init these
-	row_index = SKIP_ROWS
-	snp_index = 0
-	
-	# iterate rows until the end
-	for row_index in range(SKIP_ROWS, len(ods)):
+    if len(sys.argv) != 3:
+        print "";
+        print "Usage: X32Snippets.py <ods_file_name> <show_name>"
+        sys.exit(0)
 
-		# get scene
-		scene = ods_cell(ods, row_index, SCENE_COL)
-		if type(scene) == float:
-			scene = str(int(scene))
-		else:
-			scene = str(scene)
-		
-		# skip rows with no scene
-		if scene == '':
-			continue
-			
-		# get cue
-		cue = str(ods_cell(ods, row_index, CUE_COL))
-		
-		# report scene
-		print "Found new Scene '" + scene + "'" + " Cue '" + cue + "'"
-			
-		# open snippet file
-		snp_file = open(show_name + '.' + str(snp_index).zfill(3) + '.snp', 'w')
-		
-		# start snippet
-		snp_file.write('#2.1# "' + cue.replace('"', '') + '" 0 0 0 0 0\n')
-		
-		# first we find which channels in this cue have any DCA assignment and therefore need to be unmuted
-		cue_mutes = []
-		for chan in range(0, NUM_CHANS):
-			dca = str(ods_cell(ods, row_index, chan + FIRST_CHAN_COL))
-			if dca != '':
-				cue_mutes.append(False)
-			else:
-				cue_mutes.append(True)
-				
-		# then we write out mute-ons for channels which have become muted
-		for chan in range(0, NUM_CHANS):
-			if cue_mutes[chan]:
-				snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/mix/on OFF\n')
+    #
+    # get command line parameters
+    #
 
-		# then we write out the new DCA assignments
-		for chan in range(0, NUM_CHANS):
-			dca = str(ods_cell(ods, row_index, chan + FIRST_CHAN_COL))
-			bitmap = 0
-			if dca != '':
-				bitmap = 1 << (int(float(dca)) - 1)
-			else:
-				bitmap = 0
-			snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/grp/dca ' + str(bitmap) + '\n')
-		
-		# then we write out mute-offs for channels which have become un-muted
-		for chan in range(0, NUM_CHANS):
-			if not cue_mutes[chan]:
-				snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/mix/on ON\n')
+    ods_file_name = sys.argv[1]
+    show_name = sys.argv[2]
 
-		# finally we write out the new DCA labels
-		for dca in range(0, NUM_DCAS):
-			label = str(ods_cell(ods, row_index, dca + FIRST_DCA_COL))
-			if label == 'WARN':
-				snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
-				snp_file.write('/dca/' + str(dca + 1) + '/config/color BL\n')
-			elif label != '':
-				snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + label + '"\n')
-				snp_file.write('/dca/' + str(dca + 1) + '/config/color WH\n')
-			else:
-				snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
-				snp_file.write('/dca/' + str(dca + 1) + '/config/color OFF\n')
+    #
+    # process file
+    #
 
-		# close file
-		snp_file.close()
+    # read the file
+    ods = get_data(ods_file_name)[SHEET_NAME]
 
-		# the end?
-		if scene == "END":
-			break
-			
-		# next
-		snp_index = snp_index + 1
-	
-	# all done
-	print "Goodbye!"
+    # init these
+    row_index = SKIP_ROWS
+    snp_index = 0
+    cue_list = []
+
+    # iterate rows until the end
+    for row_index in range(SKIP_ROWS, len(ods)):
+
+        # get snippet number
+        snippet_number = ods_cell(ods, row_index, SNIPPET_NUMBER_COL)
+        if type(snippet_number) == float:
+            snippet_number = str(int(snippet_number))
+        else:
+            snippet_number = str(snippet_number)
+
+        # skip rows with no snippet
+        if snippet_number == '':
+            continue
+
+        # get snippet name
+        snippet_name = str(ods_cell(ods, row_index, SNIPPET_NAME_COL))
+
+        # get cue info
+        cue_number = str(ods_cell(ods, row_index, CUE_NUMBER_COL))
+        if '' != cue_number:
+            cue = Cue(cue_number, snippet_name, snp_index)
+            cue_list.append(cue)
+
+        # report snippet
+        print "Found snippet %s: %s" % (snippet_number, snippet_name)
+
+        # open snippet file
+        snp_file = open(show_name + '.' + str(snp_index).zfill(3) + '.snp', 'w')
+
+        # start snippet
+        snp_file.write('#2.1# "' + snippet_name.replace('"', '') + '" 0 0 0 0 0\n')
+
+        # first we find which channels in this snippet have any DCA assignment and therefore need to be unmuted
+        snippet_mutes = []
+        for chan in range(0, NUM_CHANS):
+            dca = str(ods_cell(ods, row_index, chan + FIRST_CHAN_COL))
+            if dca != '':
+                snippet_mutes.append(False)
+            else:
+                snippet_mutes.append(True)
+
+        # then we write out mute-ons for channels which have become muted
+        for chan in range(0, NUM_CHANS):
+            if snippet_mutes[chan]:
+                snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/mix/on OFF\n')
+
+        # then we write out the new DCA assignments
+        for chan in range(0, NUM_CHANS):
+            dca = str(ods_cell(ods, row_index, chan + FIRST_CHAN_COL))
+            bitmap = 0
+            if dca != '':
+                bitmap = 1 << (int(float(dca)) - 1)
+            else:
+                bitmap = 0
+            snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/grp/dca ' + str(bitmap) + '\n')
+
+        # then we write out mute-offs for channels which have become un-muted
+        for chan in range(0, NUM_CHANS):
+            if not snippet_mutes[chan]:
+                snp_file.write('/ch/' + str(chan + 1).zfill(2) + '/mix/on ON\n')
+
+        # finally we write out the new DCA labels
+        for dca in range(0, NUM_DCAS):
+            label = str(ods_cell(ods, row_index, dca + FIRST_DCA_COL))
+            if label == 'WARN':
+                snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
+                snp_file.write('/dca/' + str(dca + 1) + '/config/color BL\n')
+            elif label != '':
+                snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + label + '"\n')
+                snp_file.write('/dca/' + str(dca + 1) + '/config/color WH\n')
+            else:
+                snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
+                snp_file.write('/dca/' + str(dca + 1) + '/config/color OFF\n')
+
+        # close file
+        snp_file.close()
+
+        # the end?
+        if snippet_number == "END":
+            break
+
+        # next
+        snp_index += 1
+
+    # add the cues to the .shw file
+    if 0 < len(cue_list):
+        # read the existing .shw file
+        shw_filename = show_name + '.shw'
+        try:
+            with open(shw_filename, mode='r') as shw:
+                shw_lines = shw.readlines()
+            # write it back out, inserting the cue lines into it
+            with open(shw_filename, mode='w') as shw:
+                for i in range(0, len(shw_lines)):
+                    if 'cue/' != shw_lines[i][0:4]:
+                        # delete all existing cues
+                        shw.write(shw_lines[i])
+                    if 'show ' == shw_lines[i][0:5]:
+                        # insert new cues
+                        for c in range(0, len(cue_list)):
+                            cue = cue_list[c]
+                            print 'Created cue %s: %s' % (cue.cue_number, cue.cue_name)
+                            shw.write(cue.shw_file_line(c))
+        except IOError:
+            print 'ERROR: Cannot add cues to .shw file. Assure that %s exists and is writable' % (shw_filename,)
+
+    # all done
+    print "Goodbye!"
